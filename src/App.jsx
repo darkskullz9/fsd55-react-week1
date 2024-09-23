b90cdd25fc4d6fcecbb052ab329e449c57ea5830
darkskullz9 <djskullz971@gmail.com>
2024-09-23 14:43:59 +0200

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import User from './User.jsx'
+
+function App() {
+  return (
+    <>
+      <h1>Hello</h1>
+      <User/>
+    </>
+  )
+}
+
+export default App
---
+++ src/User.jsx
@@ -0,0 +1,10 @@
+const User = () => {
+    return (
+        <>
+            <h1>Steven</h1>
+            <p>Dev Front-End</p>
+        </>
+    );
+}
+
+export default User
